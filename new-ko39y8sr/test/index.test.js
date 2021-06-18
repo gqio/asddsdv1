4c68a61bd89c87986bdc6101b9e1961a1431027c
Gregory Quinio <quinio.gregory@gmail.com>
2021-06-18 20:30:09 +0000

    First commit

---
+++ new-ko39y8sr/test/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import expect from 'expect';
+import { render } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import userEvent from '@testing-library/user-event';
+
+describe('test', () => {
+  it('should be true', () => {
+    // const props = jest.fn();
+    // const { container } = render(
+    //   <Form
+    //     placeholder="Renseigner"
+    //     labelText="Nom"
+    //     type="default"
+    //     value=""
+    //     width="sm"
+    //     onChange={(value) => console.log(value)}
+    //   />
+    // );
+    expect(null).toBe(null);
+  });
+});
